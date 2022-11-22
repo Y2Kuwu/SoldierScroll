4b41a08f2d39f0aecbea171ebcb53c2858f11367
Y2Kuwu <jamesmfox5@gmail.com>
2022-11-21 16:26:00 -0800

    main in work

--- main.cpp
+++ main.cpp
@@ -4,7 +4,40 @@
 
 int main(){
     
-    Tex tex;
-    View view;
-    //view.makeSprite();
+    sf::RenderWindow win( sf::VideoMode( 800,600 ),"SFML window" );
+
+	{
+		Controller actor( { 100.0f,100.0f } );
+		Controller actorClone( { 100.0f,200.0f } );
+	}
+
+    Tex::ErasePrev();
+
+    Controller actor ({ 100.0f,100.0f });
+
+    auto timePtr = std::chrono::steady_clock::now();
+    
+    while( win.isOpen() )
+	{
+		// Process events
+		sf::Event event;
+		while( win.pollEvent( event ) )
+		{
+			// Close window: exit
+			if( event.type == sf::Event::Closed )
+				win.close();
+		}
+
+    float delta;
+    {
+        const auto timePtrClone = std::chrono::steady_clock::now();
+        delta = std::chrono::duration<float>( timePtrClone - timePtr).count();
+        timePtr = timePtrClone;
+    }
+
+    win.clear();
+    actor.timingBelt(delta);
+    win.display();
+}
+return EXIT_SUCCESS;
 }
