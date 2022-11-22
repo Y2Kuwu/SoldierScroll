4ea867907c13da56124ede511b44a0bb0a6025bb
Y2Kuwu <jamesmfox5@gmail.com>
2022-11-21 16:28:16 -0800

    error sorting

--- central.cpp
+++ central.cpp
@@ -1,10 +1,10 @@
-#include "central.h"
+// #include "central.h"
 
-Central::Central() 
-{
-    renWin = win;
-    health = 100;
-    armor = 0;
-    ammo = 200;
-    //weapon = tbd
-}
+// Central::Central() 
+// {
+//     renWin = win;
+//     health = 100;
+//     armor = 0;
+//     ammo = 200;
+//     //weapon = tbd
+// }
